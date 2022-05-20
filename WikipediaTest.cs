6d719f79a8ef32fad5f37ee2e955029f9fc17789
Mihail Mihaylov <Mihail Mihaylov@DESKTOP-029GOQJ>
2022-05-20 11:05:02 +0100

    Add project files.

---
+++ WikipediaTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+// Create new Chrome broeser instances
+var driver = new ChromeDriver();
+
+//Navigate to Wikipedia
+driver.Url = "https://wikipedia.org";
+
+Console.WriteLine("CURRENT TITLE:  " + driver.Title);
+
+//locate search field
+var searchField = driver.FindElement(By.Id("searchInput"));
+searchField.Click();
+searchField.SendKeys("QA" + Keys.Enter);
+
+Console.WriteLine("TITLE AFTER SEARCH :  " + driver.Title);
+
+//Close browser
+driver.Quit();
+
